6a8f5043bff5d16b64644a4bb8cc630da09be466
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-01-21 01:49:14 +0600

    46_5-1 Recap Create react app,Component, styles, props

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Jela name="Dhaka"></Jela>
+     <Jela name="Chottogram"></Jela>
     </div>
   );
 }
+
+const distric = {
+  background:'yellow'
+}
+
+function Jela(props){
+  return(
+    <div style={distric}>
+      <h2>Jela:{props.name}</h2>
+    </div>
+  )
+}
+
 
 export default App;
