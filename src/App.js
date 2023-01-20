800281761552e39f9eae557279c885aa9efb00d5
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-01-21 02:30:19 +0600

    46_5-3 Load data using the useEffect and display data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Power from './component/Power';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   return (
@@ -9,11 +11,39 @@ function App() {
      <Jela name="Chottogram"></Jela>
       <Power name="Dhaka"></Power>
       <Power name="Chottogram"></Power>
+      <LoadPost></LoadPost>
     </div>
   );
 }
 
- 
+ function LoadPost(){
+
+  const [post,setPost] = useState([])
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(res => setPost(res))
+  },[])
+
+
+  return(
+    <div>
+      {
+        post.map(singlePost => <SinglePost post={singlePost} key={singlePost.id}></SinglePost> )
+      }
+    </div>
+  )
+ }
+
+ const SinglePost = (props)=>{
+  return(
+    <div className='SinglePost'>
+      <h2>Title:{props.post.title}</h2>
+      <p>Body: {props.post.body}</p>
+    </div>
+  )
+ }
 
 const distric = {
   background:'yellow'
